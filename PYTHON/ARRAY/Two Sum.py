e4e379bd295d74a7dc22e2b2dd9e91f357a439fa
Shivakumar V Hulipalled <60393577+shivakumar20@users.noreply.github.com>
2025-02-08 07:59:09 +0530

    Update Two Sum.py

--- PYTHON/ARRAY/Two Sum.py
+++ PYTHON/ARRAY/Two Sum.py
@@ -1,1 +1,16 @@
+# Leetcode link : https://leetcode.com/problems/two-sum/
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
 
+        prevMap = {}
+
+        for i ,n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff],i]
+            prevMap[n] = i
+
+        # if (target - nums[-1]) not in prevMap:
+        #     return None
+
+        
